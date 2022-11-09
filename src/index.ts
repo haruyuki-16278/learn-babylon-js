0e1166a30aef8b2e05425cfde757646be1314fb7
haruyuki_16278 <haruyuki.program@gmail.com>
2022-11-09 15:59:54 +0900

    prepare tutorial

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,13 @@ import 'babylonjs-loaders'
   const engine = new BABYLON.Engine(canvas, true)
 
   const createScene = async function () {
-    var scene = new BABYLON.Scene(engine)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene)
+    const scene = new BABYLON.Scene(engine)
+    const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene)
 
     camera.setTarget(BABYLON.Vector3.Zero())
     camera.attachControl(canvas, true)
-
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene)
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene)
     light.intensity = 0.7
-
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene)
-    sphere.position.y = 1
-
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene)
 
     return scene
   }
