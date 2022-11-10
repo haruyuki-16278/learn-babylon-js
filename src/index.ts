631725bfb3421f501d504b39c3faecf2260b9d1e
haruyuki_16278 <haruyuki.program@gmail.com>
2022-11-10 16:38:40 +0900

    tutorial 2-2

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,19 @@ import 'babylonjs-loaders'
 
   const createScene = async function () {
     const scene = new BABYLON.Scene(engine)
-    const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene)
+    const camera = new BABYLON.ArcRotateCamera("camera1", Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0,), scene)
 
     camera.setTarget(BABYLON.Vector3.Zero())
     camera.attachControl(canvas, true)
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene)
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene)
     light.intensity = 0.7
 
-    BABYLON.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "both_houses_scene.babylon")
-      .then((result) => {
-        result.meshes[1].position.y = 5
-        result.meshes.forEach(item => {
-          console.log(item.name)
-        })
-      })
+    const ground = BABYLON.CreateGround("ground", {width: 10, height: 10}, scene)
+
+    const sound = new BABYLON.Sound("sound", "campfire.mp3", scene, null, {loop: true, autoplay: true})
+
+    const box = BABYLON.CreateBox("box", {}, scene)
+    box.position.y = 0.5
 
     return scene
   }
